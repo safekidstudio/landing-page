54431c045fbf9b446a22008cefc6a95dd505da90
phuoctaiismee <113773744+phuoctaiismee@users.noreply.github.com>
2026-07-30 10:25:25 +0700

    feat: add API route to handle Prismic cache revalidation

--- src/app/api/revalidate/route.ts
+++ src/app/api/revalidate/route.ts
@@ -1,7 +1,13 @@
 import { revalidateTag } from "next/cache";
-import { NextResponse } from "next/server";
+import { type NextRequest, NextResponse } from "next/server";
 
 export async function GET() {
+  revalidateTag("prismic", "max");
+
+  return NextResponse.json({ revalidated: true, now: Date.now() });
+}
+
+export async function POST() {
   revalidateTag("prismic", "max");
 
   return NextResponse.json({ revalidated: true, now: Date.now() });
